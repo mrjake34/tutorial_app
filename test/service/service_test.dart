b1fc98a0736a162a67d63af3605e63045227c467
Alkan Ataş <alkanatas34@gmail.com>
2024-02-27 11:07:39 +0300

    Fix showSnackBar return type and add Photo Service test

--- test/service/service_test.dart
+++ test/service/service_test.dart
@@ -3,18 +3,22 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tutorial_app/feature/home/model/user.dart';
 import 'package:tutorial_app/feature/home/service/home_service.dart';
+import 'package:tutorial_app/feature/photos/model/photos.dart';
+import 'package:tutorial_app/feature/photos/service/photos_service.dart';
 
 void main() {
   /// [HomeService] sınıfından bir nesne oluşturulur.
   /// Bu nesne, testlerde kullanılmak üzere oluşturulur.
   /// late eklenerek bu nesnenin ileride tanımlanacağı belirtilir.
   late final HomeService homeService;
+  late final PhotosService photosService;
 
   /// Testler çalışmadan önce çalıştırılacak kod bloğu.
   /// Bu blok içerisinde, testlerde kullanılacak olan nesneler oluşturulur.
   setUp(() {
     /// [homeService] değişkenine [HomeService] sınıfı atanır.
     homeService = HomeService();
+    photosService = PhotosService();
   });
 
   test('Home Service', () async {
@@ -26,6 +30,17 @@ void main() {
     /// test başarılıdır.
     expect(response.statusCode, HttpStatus.ok);
     expect(response.users, isNotNull);
-    expect(response.users!.cast<User>(), isNotEmpty);
+    expect(response.users?.cast<User>(), isNotEmpty);
+  });
+  test('Photo Service', () async {
+    /// [HomeService] sınıfının [getUsers] metodu çağrılır.
+    final response = await photosService.getPhotos(0, 10);
+
+    /// Eğer [response.statusCode] 200 ise, [response.users] null değilse
+    /// ve [response.users] içerisinde [User] sınıfından nesneler varsa
+    /// test başarılıdır.
+    expect(response.statusCode, HttpStatus.ok);
+    expect(response.data?.photos, isNotNull);
+    expect(response.data?.photos?.cast<Photos>(), isNotEmpty);
   });
 }
